7b0428c6d19481d0f9e5198f7d068dab5f1411c7
Danny2001Alonso <danny2001alejandro@gmail.com>
2023-04-14 16:57:00 -0500

    ejemplo 2

---
+++ validarRegistro.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',()=>{
+    const nombre = document.getElementById("name");
+    const phone = document.getElementById("phone");
+    const email = document.getElementById("email");
+    const pass = document.getElementById("pass");
+    const form = document.getElementById("form");
+    const parrafo = document.getElementById("warnings");
+    })
